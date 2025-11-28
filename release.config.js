a4760ee9f547e301cda06cd5ff11ee579dda847a
dheerajy <yssdhiraj@gmail.com>
2025-11-28 23:32:29 +0530

    chore: update release config and patch release yml

--- release.config.js
+++ release.config.js
@@ -1,17 +1,9 @@
 export default {
-  branches: ["main", "test-release"],
-  tagFormat: 'v${version}',
+  branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
-    ["@semantic-release/npm", { npmPublish: false }],
-    // Conditionally include git plugin only for main branch
-    ...(process.env.IS_PROD === 'true' ? [
-      ["@semantic-release/git", {
-        assets: ["CHANGELOG.md", "package.json"],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }]
-    ] : [])
+    ["@semantic-release/npm", { npmPublish: false }]
   ]
 };
