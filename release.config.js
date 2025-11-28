a966c09c55e63f9a326ee8ed94a900168de661cd
dheerajy <yssdhiraj@gmail.com>
2025-11-28 23:04:42 +0530

    chore: update config for test tagging

--- release.config.js
+++ release.config.js
@@ -1,5 +1,6 @@
 export default {
-  branches: ["main"],
+  branches: ["main", "test-release"],
+  tagFormat: process.env.IS_PROD ? 'v${version}' : null,
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
